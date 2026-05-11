ff320eebe4303a866de21c18278d2977f28c327b
Fjörðe <158854036+Fjoerde@users.noreply.github.com>
2026-05-12 00:10:26 +0100

    added state reconstruction at intersection faces between cells

---
+++ src/fluid/grid/reconstruction.cpp
@@ -0,0 +1,96 @@
+#include <cmath>
+#include <vector>
+#include <array>
+#include <memory>
+#include "../recon.hpp"
+#include "../grid.hpp"
+
+// this document contains the methods for primitive left and right
+// state reconstruction at cell faces.
+
+// get index offsets
+static const prim& getW(const patch& p, int i, int j, int k, int dim, int off) {
+    int di = (dim==0)*off; int dj = (dim==0)*off; int dk = (dim==0)*off;
+    return p.cell_(i+di,j+dj,k+dk).W;
+}
+// limiter choice switch
+static double lim_(double dl, double dr, limiter lim) {
+    switch(lim) {
+        case limiter::minmod : return minmod(dl,dr);
+        case limiter::vanleer : return vanleer(dl,dr);
+        case limiter::mclim : return mclim(dl,dr);
+        default : return minmod(dl,dr);
+    }
+}
+
+// scalar reconstruction
+static std::pair<double,double> recon_scal(double Wm, double Wc, double Wp, double Wpp, limiter lim) {
+    // left face
+    double dLc = Wc-Wm; double dRc = Wp-Wc;
+    double cslop = lim_(dLc,dRc,lim);
+    double faceL = Wc+cslop/2;
+    // right face
+    double dLp = Wp-Wc; double dRp = Wpp-Wp;
+    double pslop = lim_(dLp,dRp,lim);
+    double faceR = Wp+pslop/2;
+    return {faceL,faceR};
+}
+// reconstruction of face states of primitives
+facestate reconfp(const patch& p, int i, int j, int k, int dim, limiter lim) {
+    // gather extra cells for stencil
+    const prim& Wm = getW(p,i,j,k,dim,-1);
+    const prim& Wc = getW(p,i,j,k,dim,0);
+    const prim& Wp = getW(p,i,j,k,dim,1);
+    const prim& Wpp = getW(p,i,j,k,dim,2);
+    facestate fs;
+
+    // density, forcing positivity with ln(\rho) since \exp(ln\rho)>0
+    double km = std::log(Wm.rho);
+    double kc = std::log(Wc.rho);
+    double kp = std::log(Wp.rho);
+    double kpp = std::log(Wpp.rho);
+    auto [sL, sR] = recon_scal(km,kc,kp,kpp,lim);
+    fs.L.rho = std::exp(sL); fs.R.rho = std::exp(sR);
+    // energy with the same logarithm-exponential positivity forcing
+    double km = std::log(Wm.eps);
+    double kc = std::log(Wc.eps);
+    double kp = std::log(Wp.eps);
+    double kpp = std::log(Wpp.eps);
+    auto [sL, sR] = recon_scal(km,kc,kp,kpp,lim);
+    fs.L.eps = std::exp(sL); fs.R.eps = std::exp(sR);
+
+    // velocity, reconstructed directly
+    for(int i=0; i<3; i++) {
+        auto [sL, sR] = recon_scal(Wm.v[i],Wc.v[i],Wp.v[i],Wpp.v[i],lim);
+        fs.L.v[i] = sL; fs.R.v[i] = sR;
+    }
+    // causality check that v < c
+    auto causalv = [](prim& W, const cell& c) {
+        double v2 = 0.0;
+        metriccomp mc = c.mtr.comp(c.r,c.th);
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                v2 += mc.gam[i][j]*W.v[i]*W.v[j];
+            }
+        }
+        // if v > c, scale to just barely be causal velocity
+        if(v2>=1.0) {
+            double s = (1-1e-8)/std::sqrt(v2);
+            for(int i=0; i<3; i++) {W.v[i] *= s;};
+        }
+    };
+    causalv(fs.L,p.cell_(i,j,k)); causalv(fs.R,p.cell_(i+1*(dim==0),j+1*(dim==1),k+1*(dim==2)));
+
+    // magnetic field reconstructed to maintain zero divergence
+    for(int i=0; i<3; i++) {
+        if(i==dim) {
+            // no normal component reconstruction; overwrite later
+            fs.L.B[i] = Wc.B[i]; fs.R.B[i] = Wp.B[i];
+        } else {
+            auto [sL, sR] = recon_scal(Wm.v[i],Wc.v[i],Wp.v[i],Wpp.v[i],lim);
+            fs.L.B[i] = sL; fs.R.B[i] = sR;
+        }
+    }
+
+    return fs;
+}
